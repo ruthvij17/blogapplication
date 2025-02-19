9357b5c2cbf39662bdffee6c8afabf284edbbf6d
ChethanTN7 <chethantn947@gmail.com>
2025-02-19 17:34:38 +0530

    added routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "./Pages/HomePage";
+import LoginPage from "./Pages/LoginPage";
+import SigninPage from "./Pages/SigninPage";
+import BlogPage from "./Pages/BlogPage";
+import AddBlogPage from "./Pages/AddBlogPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/signin" element={<SigninPage />}></Route>
+      <Route path="/blog/:id" element={<BlogPage />}></Route>
+      <Route path="/add/blog" element={<AddBlogPage />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
